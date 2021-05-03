7c2a4463ae6a50c315d124e3a9c0c06f08184fca
alex12-max <79738038+alex12-max@users.noreply.github.com>
2021-05-04 01:11:09 +0300

    t

--- templates/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $url='localhost';
-    $username='root';
-    $password='';
-    $con=mysqli_connect($url,$username,$password,"hotel");
-    if($con === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
